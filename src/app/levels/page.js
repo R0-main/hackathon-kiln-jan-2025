e1efdb008861d4959ce3a39612f5e53aa1bc5211
R0-main <justromixx@gmail.com>
2025-02-01 13:56:19 +0100

    added things

--- src/app/levels/page.js
+++ src/app/levels/page.js
@@ -1,11 +1,23 @@
+"use client"
+
 import "../globals.css";
 import Image from "next/image";
 import KilnLogo from '../../../public/kiln-logo.jpg';
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 
 export default function Level({ children }) {
+	const [level, setLevel] = useState(1);
+
+	useEffect(() => {
+		const levelValue = localStorage.getItem('level') || 0;
+		setLevel(Number(levelValue));
+	}, [])
+
 	return (
 		<div className="flex justify-center items-center align-middle w-[100vw] h-[100vh] flex-col">
+			{level}
 			<div className="flex justify-center align-middle flex-row w-full h-full ">
 				<div className="flex justify-center items-center h-full cursor-pointer [transform:perspective(800px)_rotateY(10deg)] hover:[transform:perspective(800px)_rotateY(-10deg)] w-full sm:w-84 p-5 rounded-lg shadow-lg group duration-300">
 					<div className="card bg-base-100 image-full left-[-75px]  h-[85%] shadow-xl">
