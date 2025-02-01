7b04aa1f8f61fe46f4be7101212d248912b460f2
R0-main <justromixx@gmail.com>
2025-02-01 09:56:04 +0100

    fqwf

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,24 @@ import Level from "./levels/level";
 
 export default function Home() {
   return (
-    <div className="flex">
-     <button className="btn btn-primary">Button</button>
+    <div className="flex justify-center align-middle w-full h-full flex-col">
+      <div className="flex flex-row w-full h-full ">
+        <div className="w-full h-full">
+          <button className="btn btn-primary"> </button>
+        </div>
+        <div className="w-full h-full">
+          <button className="btn btn-primary"> </button>
+        </div>
+      </div>
+      <div className="flex flex-row w-full h-full">
+        <div className="w-full h-full">
+          <button className="btn btn-primary"> </button>
+        </div>
+        <div className="w-full h-full">
+          <button className="btn btn-primary"> </button>
+        </div>
+      </div>
+
     </div>
   );
 }
