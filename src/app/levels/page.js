859bbd2dc8adf396a5455f503bc449ad234facae
R0-main <justromixx@gmail.com>
2025-02-01 12:56:34 +0100

    working progress

---
+++ src/app/levels/page.js
@@ -0,0 +1,41 @@
+import "../globals.css";
+import Image from "next/image";
+import KilnLogo from '../../../public/kiln-logo.jpg';
+
+
+export default function Level({ children }) {
+	return (
+		<div className="flex justify-center items-center align-middle w-[100vw] h-[100vh] flex-col">
+			<div className="flex justify-center align-middle flex-row w-full h-full ">
+				<div className="flex justify-center items-center h-full cursor-pointer [transform:perspective(800px)_rotateY(10deg)] hover:[transform:perspective(800px)_rotateY(-10deg)] w-full sm:w-84 p-5 rounded-lg shadow-lg group duration-300">
+					<div className="card bg-base-100 image-full left-[-75px]  h-[85%] shadow-xl">
+						<figure>
+							<img
+								src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+								alt="Shoes" />
+						</figure>
+						<div className="card-body">
+							<h2 className="card-title">Shoes!</h2>
+							<p>If a dog chews shoes whose shoes does he choose?</p>
+							<div className="card-actions justify-end">
+								<button className="btn btn-primary">Buy Now</button>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="flex justify-center items-center w-full h-full">
+					<button className="btn btn-primary"> </button>
+				</div>
+			</div>
+			<div className="flex flex-row w-full h-full">
+				<div className="flex justify-center items-center w-full h-full">
+					<button className="btn btn-primary"> </button>
+				</div>
+				<div className="flex justify-center items-center w-full h-full">
+					<button className="btn btn-primary"> </button>
+				</div>
+			</div>
+			<Image src={KilnLogo} alt="KilnLogo" width={300} height={300} priority className="absolute" />
+		</div>
+	);
+}
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,29 @@
 import daisyui from 'daisyui'
+
 const config = {
- content: [
-   './pages/**/*.{js,ts,jsx,tsx,mdx}',
-   './components/**/*.{js,ts,jsx,tsx,mdx}',
-   './src/app/**/*.{js,ts,jsx,tsx,mdx}',
- ],
- theme: {
- },
- daisyui: {
-  themes: ["autumn"],
-},
- plugins: [daisyui],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'bounce-slow': 'bounce 1s infinite',
+      },
+    }
+  },
+  daisyui: {
+    themes: [
+      {
+        luxury: {
+          ...require("daisyui/src/theming/themes")["luxury"],
+          "primary": "#ff8000", // Remplace la couleur primaire ici
+        },
+      },
+    ],
+  },
+  plugins: [daisyui],
 }
+
 export default config
